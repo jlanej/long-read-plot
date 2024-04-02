4c6c1f393d14b0b9e9ce7673876f5fffaafca8e6
jlanej <jlanej@gmail.com>
2024-04-02 12:17:08 -0500

    stub method to load and parse alignments

--- bamRUtils.R
+++ bamRUtils.R
@@ -1,5 +1,10 @@
+# for some reason crayon seems to install just fine in the docker image and can be loaded during image creation.
+# But after the image is created, it can't be loaded. So, this installs it in case it's not already installed.
+if (!require(crayon))
+  install.packages("crayon")
+library(crayon)
 require(Rsamtools)
-
+require(GenomicAlignments)
 
 
 .unlist <- function (x)
@@ -27,4 +32,29 @@ loadBam <- function(bamFile, param) {
   bam = scanBam(bamFile, param = param)
   bam = convertBam(bam = bam, param = param)
   return(bam)
+}
+
+loadAndParseAlignments <- function(bamFile, region) {
+  which <-
+    GRanges(seqnames  = region[1],
+            ranges = IRanges(as.integer(region[2]), as.integer(region[3])))
+  bamAll = loadBam(bamFile, param = ScanBamParam(what = scanBamWhat(), which = which))
+  bamAll$flag = as.character(bamAll$flag)
+  bamAll$sequenceLength = nchar(bamAll$seq)
+  bamAll$swidth = cigarWidthAlongReferenceSpace(bamAll$cigar)
+  bamAll$end = bamAll$pos + bamAll$swidth
+  bamAll$proportionOfReadAlignedToRef = cigarWidthAlongQuerySpace(bamAll$cigar, after.soft.clipping = TRUE) / bamAll$sequenceLength
+  bamAll$impliedEnd = bamAll$pos + bamAll$sequenceLength
+  bamAll$diff = bamAll$impliedEnd - bamAll$end
+  bamAll$op = explodeCigarOps(bamAll$cigar)
+  bamAll$len = explodeCigarOpLengths(bamAll$cigar)
+  bamAll$firstOp = sapply(bamAll$op, function(x)
+    x[1])
+  bamAll$lastOp = sapply(bamAll$op, function(x)
+    x[length(x)])
+  bamAll$firstLen = sapply(bamAll$len, function(x)
+    x[1])
+  bamAll$lastLen = sapply(bamAll$len, function(x)
+    x[length(x)])
+  return(bamAll)
 }
--- longReadPlot.R
+++ longReadPlot.R
@@ -1,9 +1,33 @@
-print("loading libraries")
-print("loading optparse")
 library(optparse)
-print("loading crayon")
-if(!require(crayon)) install.packages("crayon")
-library(crayon)
-
 source("bamRUtils.R")
-print("hi")
+# options for the bam input file, image output file, and the region to plot in ucsc format
+option_list = list(
+  make_option(
+    c("-i", "--bam"),
+    type = "character",
+    default = "./examples/NA19240_2020_merged.ccs.hg38.aligned.chr17_10958130_11017414.bam",
+    help = "bam file to plot",
+    metavar = "file"
+  ),
+  make_option(
+    c("-o", "--output"),
+    type = "character",
+    default = "./examples/NA19240_2020_merged.ccs.hg38.aligned.chr17_10958130_11017414.png",
+    help = "output image file",
+    metavar = "file"
+  ),
+  make_option(
+    c("-r", "--region"),
+    type = "character",
+    default = "chr17:10958130-11017414",
+    help = "region to plot in ucsc format",
+    metavar = "region"
+  )
+)
+
+opt_parser = OptionParser(option_list = option_list)
+opt = parse_args(opt_parser)
+bamFile = opt$bam
+# parse the region to plot to Granges
+region = strsplit(opt$region, ":|-")[[1]]
+bamAll = loadAndParseAlignments(bamFile, region)
