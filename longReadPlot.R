5520e8c71547674eca22f97c3cb6e2cb4171b817
jlanej <jlanej@gmail.com>
2024-04-04 10:32:08 -0500

    update examples

--- longReadPlot.R
+++ longReadPlot.R
@@ -26,7 +26,7 @@ option_list = list(
   make_option(
     c("-d", "--debug"),
     action = "store_true",
-    default = TRUE,
+    default = FALSE,
     help = "debug mode, creates a tsv file of the alignments",
     dest = "debug"
   )
